4378bdf608c2d63330592c2ac5b7b0d6792d56ce
xonze（祥哥） <xonze@126.com>
2017-04-18 16:06:33 +0800

    Holographic DirectX 11 App (Universal Windows)  program template

---
+++ Holographic-DirectX-11-App/AppViewSource.cs
@@ -0,0 +1,13 @@
+﻿using Windows.ApplicationModel.Core;
+
+namespace Holographic_DirectX_11_App
+{
+    // The entry point for the app.
+    internal class AppViewSource : IFrameworkViewSource
+    {
+        public IFrameworkView CreateView()
+        {
+            return new AppView();
+        }
+    }
+}
